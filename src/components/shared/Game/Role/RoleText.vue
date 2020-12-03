f5f2f0c86ce97cfd32a8412e2cd9f4361ca7bd57
antoinezanardi <antoine.zanardi@epitech.eu>
2020-12-03 21:31:29 +0100

    refactor(Roles): RoleText.vue refactored to be more concise

--- src/components/shared/Game/Role/RoleText.vue
+++ src/components/shared/Game/Role/RoleText.vue
@@ -13,28 +13,7 @@ export default {
     },
     computed: {
         roleText() {
-            const roleText = {
-                "back": undefined,
-                "guard": this.$t("Role.the.guard"),
-                "hunter": this.$t("Role.the.hunter"),
-                "raven": this.$t("Role.the.raven"),
-                "seer": this.$t("Role.the.seer"),
-                "villager": this.$t("Role.the.villager"),
-                "werewolf": this.$t("Role.the.werewolf"),
-                "werewolves": this.$t("Role.the.werewolves"),
-                "witch": this.$t("Role.the.witch"),
-                "sheriff": this.$t("Role.the.sheriff"),
-                "little-girl": this.$t("Role.the.little-girl"),
-                "villager-villager": this.$t("Role.the.villager-villager"),
-                "cupid": this.$t("Role.the.cupid"),
-                "two-sisters": this.$t("Role.the.two-sisters"),
-                "three-brothers": this.$t("Role.the.three-brothers"),
-                "wild-child": this.$t("Role.the.wild-child"),
-                "dog-wolf": this.$t("Role.the.dog-wolf"),
-                "big-bad-wolf": this.$t("Role.the.big-bad-wolf"),
-                "all": this.$t("Role.the.all"),
-            };
-            return this.role && roleText[this.role] ? roleText[this.role] : this.$t("RoleText.chooseARole");
+            return !this.role ? this.$t("RoleText.chooseARole") : this.$t(`Role.the.${this.role}`);
         },
         roleTextClasses() {
             if (this.role) {
